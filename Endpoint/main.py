410407f9959112dea15fcd09f8b8d31879bcad4e
biboy1999 <biboy1999@gmail.com>
2020-12-29 10:29:54 +0800

    add temp save read [server]
    update post url [endpoint]

--- Endpoint/main.py
+++ Endpoint/main.py
@@ -5,12 +5,10 @@ from json import dumps
 from random import randint
 
 
-while True:
-    # with serial.Serial("/dev/ttyUSB0", 115200) as ser:
-        # line = ser.readline()
-        # json_data = dumps(line)
-        # post("127.0.0.1", json=json_data)
-    
-    f1 = randint(0,10)
-    get("https://api.thingspeak.com/update?api_key=33MH50NFFKRC2T3Y&field1={}".format(f1))
-    sleep(2)
+with serial.Serial("/dev/ttyUSB0", 115200) as ser:
+    while True:
+        line = ser.readline()
+        json_data = dumps(line)
+        json_data["iot_id"] = 1
+        post("140.125.207.192:8000", json=json_data)
+        sleep(2)
--- Server/server.py
+++ Server/server.py
@@ -1,15 +1,49 @@
 from sanic import Sanic
 from sanic.response import json
+from sanic.log import logger
+from sanic_openapi import  swagger_blueprint, doc
+import pymysql.cursors
 
-app = Sanic("App Name")
 
-@app.get("/save")
-async def test(request):    
-    return json({"status": "fuck"})
+app = Sanic("water")
+app.blueprint(swagger_blueprint)
+# db_conn = pymysql.Connect(host="db", user="root", password="superpapaya",
+#                           db="auto_water", charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
+db_conn = pymysql.Connect(host="db", user="water", password="no_water_for_you",
+                          db="auto_water", cursorclass=pymysql.cursors.DictCursor)
+
 
 @app.get("/temp")
-async def login(request):
-    return json({"temp":"99.9"})
+async def test(request):
+    result = None
+    with db_conn.cursor() as cur:
+        sql = "SELECT * FROM `record`"
+        cur.execute(sql)
+        result = cur.fetchall()
+    return json(result)
+
+
+@app.post("/temp")
+@doc.consumes(
+    doc.JsonBody(
+        {
+            "iot_id":doc.Integer("iot's id"),
+            "temp":doc.Float("temperature"),
+            "hum":doc.Float("hum")
+        }
+    ),
+    location="body",
+)
+async def save_temp(request):
+    iot_id = request.json["iot_id"]
+    temp = request.json["temp"]
+    hum = request.json["hum"]
+    data  = (iot_id, temp, hum,)
+    with db_conn.cursor() as cur:
+        sql = "INSERT INTO `record` VALUES (null, %s, %s, %s, null)"
+        cur.execute(sql,data)
+    db_conn.commit()
+    return  json({"status": "ok"}, status=200)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
