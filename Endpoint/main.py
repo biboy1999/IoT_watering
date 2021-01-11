d3eee78610f85e03163c6a5b9ec7da7d31115644
biboy1999 <biboy1999@gmail.com>
2021-01-11 19:08:43 +0800

    fix wrong tty
    fix wrong api link

--- Endpoint/main.py
+++ Endpoint/main.py
@@ -4,11 +4,11 @@ from requests import post, get
 from json import dumps
 from random import randint
 
-with serial.Serial("/dev/ttyUSB0", 115200) as ser:
+with serial.Serial("/dev/ttyACM0", 115200) as ser:
     while True:
-        line = ser.readline()
+        line = ser.readline().deocde()
         dirt_hum, air_hum, air_temp = line.split(",")
         json_data = {}
         json_data["iot_id"] = 1
-        post("140.125.207.192:8000", json=json_data)
+        post("http://140.125.207.192:8000", json=json_data)
         sleep(2)
