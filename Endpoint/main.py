99c75b4b6e660faa388ee450c840d66562fc1a32
biboy1999 <biboy1999@gmail.com>
2021-01-12 00:21:53 +0800

    add water schedule
    fix requirement.txt not work on pi

--- Endpoint/main.py
+++ Endpoint/main.py
@@ -3,12 +3,34 @@ from time import sleep
 from requests import post, get
 from json import dumps
 from random import randint
+import schedule
 
-with serial.Serial("/dev/ttyACM0", 115200) as ser:
+schedule_table = []
+ser = serial.Serial("/dev/ttyACM0")
+def water():
+    ser.writelines("1")
+    pass
+
+if __name__ == "__main__":
     while True:
-        line = ser.readline().deocde()
-        dirt_hum, air_hum, air_temp = line.split(",")
-        json_data = {}
-        json_data["iot_id"] = 1
-        post("http://140.125.207.192:8000", json=json_data)
-        sleep(2)
+        try:
+            schedule.run_pending()
+
+            line = ser.readline().deocde().strip()
+            dirt_hum, air_hum, air_temp = line.split(",")
+            json_data = {}
+            json_data["iot_id"] = 1
+            json_data["air_temp"] = air_temp
+            json_data["air_hum"] = air_hum
+            json_data["dirt_hum"] = dirt_hum
+            post("http://140.125.207.192:8000/temp", json=json_data)
+            schedule_json = get("http://140.125.207.192:8000/schedule").json()
+            parsed_scahedule = schedule_json["water_schedule"].strip().split(";")
+            if parsed_scahedule != schedule_table:
+                schedule_table = parsed_scahedule
+                schedule.clear()
+                for time in schedule_table:
+                    schedule.every().day.at(time).do(water)
+            sleep(1)
+        except:
+            pass
